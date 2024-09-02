a19be784e1af196503ec01979aa7a28a8c63c318
UrosZikic <uroszikic1512988@gmail.com>
2024-09-02 12:12:21 +0200

    group phase elimination, group sort, quarter finals prepared

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ const groups = require(path.join(__dirname, "groups.json"));
 const exhibition = require(path.join(__dirname, "exibitions.json"));
 const calculateProbability = require("./probability");
 
-const groupPhase = function (group) {
+const groupPhase = function (group, category) {
   const [team1, team2, team3, team4] = [...group];
   const groups = [team1, team2, team3, team4];
   let forma = 75;
   let count = 0;
   const probabilityInterval = setInterval(() => {
     if (count <= 3) {
-      calculateProbability(groups[count], (count += 1), 3, group, forma);
+      calculateProbability(
+        groups[count],
+        (count += 1),
+        3,
+        group,
+        forma,
+        category
+      );
     } else {
       // console.log(groups);
       clearInterval(probabilityInterval);
     }
   }, 500);
 };
-
-groupPhase(groups.A);
-// groupPhase(groups.B);
-// groupPhase(groups.C);
+groupPhase(groups.A, "A");
+groupPhase(groups.B, "B");
+groupPhase(groups.C, "C");
--- probability.js
+++ probability.js
@@ -3,7 +3,11 @@ const path = require("path");
 const exhibition = require(path.join(__dirname, "exibitions.json"));
 const groups = require(path.join(__dirname, "groups.json"));
 
-function calculateProbability(team, count, limit, group, forma) {
+let qualifiers = [];
+let secondPlaceQualifiers = [];
+let thirdPlaceQualifiers = [];
+
+function calculateProbability(team, count, limit, group, forma, category) {
   team.winProb = team.winProb ? team.winProb : [];
   team.results = team.results ? team.results : [];
 
@@ -33,7 +37,8 @@ function calculateProbability(team, count, limit, group, forma) {
       forma,
       winProbability,
       team,
-      groupData
+      groupData,
+      category
     );
   }
 }
@@ -45,7 +50,8 @@ function calculateShape(
   forma,
   winProbability,
   team,
-  groupData
+  groupData,
+  category
 ) {
   // provera da li je ciljni tim igrao protiv tima iz grupe na domacem terenu
   exhibition[targetTeam].forEach((item) => {
@@ -69,10 +75,10 @@ function calculateShape(
 
   team.winProb = [...team.winProb, winProbability];
 
-  r(team, group, winProbability, i, groupData);
+  r(team, group, winProbability, i, groupData, category);
 }
 
-const r = (team, group, winProbability, i, groupData) => {
+const r = (team, group, winProbability, i, groupData, category) => {
   const targetTeam = team.ISOCode;
   const enemyTeam = group[i].ISOCode;
 
@@ -109,12 +115,12 @@ const r = (team, group, winProbability, i, groupData) => {
             Math.ceil(Math.random() * (100 + winProbability / 10) + 30),
           ];
   }
-  if (group[0].results.length >= 3 && group[0].ISOCode == "CAN") {
-    sortGroupPhase(group);
+  if (group[0].results.length >= 3) {
+    sortGroupPhase(group, category);
   }
 };
 
-function sortGroupPhase(group) {
+function sortGroupPhase(group, category) {
   let groupGameResults = group.map((item, i) => item.results);
 
   let groupPlacements = [
@@ -163,7 +169,7 @@ function sortGroupPhase(group) {
 
   if (group[3].results.length === 3) {
     console.log("Grupna faza - I kolo:");
-    console.log("Grupa: A:");
+    console.log(`Grupa: ${category}:`);
     for (let i = 0; i < 6; i++) {
       if (i < 3) {
         console.log(
@@ -171,6 +177,10 @@ function sortGroupPhase(group) {
             groupGameResults[i + 1][0]
           })`
         );
+        groupPlacements[0][0].Team = group[0].Team;
+        groupPlacements[0][0].FIBARanking = group[0].FIBARanking;
+        groupPlacements[i + 1][0].Team = group[i + 1].Team;
+        groupPlacements[i + 1][0].FIBARanking = group[i + 1].FIBARanking;
         if (groupGameResults[0][i] > groupGameResults[i + 1][0]) {
           groupPlacements[0][0].won += 1;
           groupPlacements[0][0].points += 2;
@@ -240,8 +250,209 @@ function sortGroupPhase(group) {
     }
   }
 
-  if (groupPlacements[3][0].won + groupPlacements[3][0].lost === 3)
-    console.log(groupPlacements);
+  if (groupPlacements[3][0].won + groupPlacements[3][0].lost === 3) {
+    const x = groupPlacements.sort((a, b) => {
+      // Compare points first
+      if (b[0].points !== a[0].points) {
+        return b[0].points - a[0].points;
+      }
+      // If points are the same, compare by difference
+      return b[0].difference - a[0].difference;
+    });
+
+    console.log(
+      ` Grupa ${category} (Ime - pobede/porazi/bodovi/postignuti koševi/primljeni koševi/koš razlika):`
+    );
+    for (let i = 0; i <= 3; i++) {
+      console.log(
+        `${i + 1}. ${x[i][0].Team}   ${x[i][0].won} / ${x[i][0].lost} / ${
+          x[i][0].points
+        } / ${x[i][0].scored} / ${x[i][0].received} / ${x[i][0].difference}`
+      );
+    }
+
+    qualifiers = [...qualifiers, x[0]];
+    secondPlaceQualifiers = [...secondPlaceQualifiers, x[1]];
+    thirdPlaceQualifiers = [...thirdPlaceQualifiers, x[2]];
+
+    if (qualifiers.length >= 3 && thirdPlaceQualifiers.length >= 3) {
+      let confirmedQualified = qualifiers.sort((a, b) => {
+        if (b[0].points !== a[0].points) {
+          return b[0].points - a[0].points;
+        }
+        return b[0].difference - a[0].difference;
+      });
+
+      let secondPlaceQualified = secondPlaceQualifiers.sort((a, b) => {
+        // Compare points first
+        if (b[0].points !== a[0].points) {
+          return b[0].points - a[0].points;
+        }
+        // If points are the same, compare by difference
+        return b[0].difference - a[0].difference;
+      });
+
+      let thirdPlaceQualified = thirdPlaceQualifiers.sort((a, b) => {
+        // Compare points first
+        if (b[0].points !== a[0].points) {
+          return b[0].points - a[0].points;
+        }
+        // If points are the same, compare by difference
+        return b[0].difference - a[0].difference;
+      });
+      // 3rd third placer will not qualify
+      qualifiers = [
+        ...confirmedQualified,
+        ...secondPlaceQualified,
+        thirdPlaceQualified[0],
+        thirdPlaceQualified[1],
+      ];
+
+      handleQualifiers(qualifiers);
+    }
+  }
+}
+function handleQualifiers(qualifier) {
+  // console.log(qualifier);
+  const originalGroup = [
+    ["Kanada", "Australija", "Grčka", "Španija"],
+    ["Nemačka", "Francuska", "Brazil", "Japan"],
+    ["Sjedinjene Države", "Srbija", "Južni Sudan", "Puerto Riko"],
+  ];
+
+  let groupD = [qualifier[0][0], qualifier[1][0]];
+  let groupE = [qualifier[2][0], qualifier[3][0]];
+  let groupF = [qualifier[4][0], qualifier[5][0]];
+  let groupG = [qualifier[6][0], qualifier[7][0]];
+
+  let eliminationPhaseMatchOne = [];
+  let eliminationPhaseMatchTwo = [];
+  let eliminationPhaseMatchThree = [];
+  let eliminationPhaseMatchFour = [];
+
+  for (let i = 0; i <= 2; i++) {
+    if (
+      originalGroup[i].includes(qualifier[0][0].Team) &&
+      originalGroup[i].includes(qualifier[7][0].Team)
+    ) {
+      eliminationPhaseMatchOne = [qualifier[0][0], qualifier[6][0]];
+      eliminationPhaseMatchTwo = [qualifier[1][0], qualifier[7][0]];
+      break;
+    } else if (
+      originalGroup[i].includes(qualifier[0][0].Team) &&
+      originalGroup[i].includes(qualifier[6][0].Team)
+    ) {
+      eliminationPhaseMatchOne = [qualifier[0][0], qualifier[7][0]];
+      eliminationPhaseMatchTwo = [qualifier[1][0], qualifier[6][0]];
+      break;
+    } else if (
+      originalGroup[i].includes(qualifier[1][0].Team) &&
+      originalGroup[i].includes(qualifier[6][0].Team)
+    ) {
+      eliminationPhaseMatchOne = [qualifier[0][0], qualifier[6][0]];
+      eliminationPhaseMatchTwo = [qualifier[1][0], qualifier[7][0]];
+      break;
+    } else if (
+      originalGroup[i].includes(qualifier[1][0].Team) &&
+      originalGroup[i].includes(qualifier[7][0].Team)
+    ) {
+      eliminationPhaseMatchOne = [qualifier[0][0], qualifier[7][0]];
+      eliminationPhaseMatchTwo = [qualifier[1][0], qualifier[6][0]];
+      break;
+    } else {
+      const randomize = Math.random * 100 + 1 > 50 ? 6 : 7;
+      eliminationPhaseMatchOne = [qualifier[0][0], qualifier[randomize][0]];
+      eliminationPhaseMatchTwo = [
+        qualifier[1][0],
+        qualifier[randomize % 2 === 0 ? 7 : 6],
+      ];
+    }
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    if (
+      originalGroup[i].includes(qualifier[2][0].Team) &&
+      originalGroup[i].includes(qualifier[3][0].Team)
+    ) {
+      const randomize = Math.random * 100 + 1 > 50 ? 4 : 5;
+      eliminationPhaseMatchThree = [qualifier[2][0], qualifier[randomize][0]];
+      eliminationPhaseMatchFour = [
+        qualifier[3][0],
+        qualifier[randomize % 2 == 0 ? 5 : 4][0],
+      ];
+      break;
+    } else if (
+      originalGroup[i].includes(qualifier[2][0].Team) &&
+      originalGroup[i].includes(qualifier[4][0].Team)
+    ) {
+      const randomize = Math.random * 100 + 1 > 50 ? 3 : 5;
+      eliminationPhaseMatchThree = [qualifier[2][0], qualifier[randomize][0]];
+      eliminationPhaseMatchFour = [
+        qualifier[4][0],
+        qualifier[randomize > 3 ? 3 : 5][0],
+      ];
+      break;
+    } else {
+      const randomize = Math.random * 100 + 1 > 50 ? 3 : 4;
+      eliminationPhaseMatchThree = [qualifier[2][0], qualifier[randomize][0]];
+      eliminationPhaseMatchFour = [
+        qualifier[5][0],
+        qualifier[randomize % 2 === 0 ? 3 : 4][0],
+      ];
+    }
+  }
+  console.log(" ");
+  console.log("Šeširi:");
+  console.log("  Šešir D");
+  console.log("    " + groupD[0].Team);
+  console.log("    " + groupD[1].Team);
+  console.log("  Šešir E");
+  console.log("    " + groupE[0].Team);
+  console.log("    " + groupE[1].Team);
+  console.log("  Šešir F");
+  console.log("    " + groupF[0].Team);
+  console.log("    " + groupF[1].Team);
+  console.log("  Šešir G");
+  console.log("    " + groupG[0].Team);
+  console.log("    " + groupG[1].Team);
+
+  const quarterFinalMatches = [
+    eliminationPhaseMatchOne,
+    eliminationPhaseMatchTwo,
+    eliminationPhaseMatchThree,
+    eliminationPhaseMatchFour,
+  ];
+
+  quarterFinals(quarterFinalMatches);
+}
+
+function quarterFinals(quarterFinalMatches) {
+  console.log(" ");
+  console.log("Eliminaciona faza:");
+  console.log(
+    "   " +
+      quarterFinalMatches[0][0].Team +
+      " - " +
+      quarterFinalMatches[0][1].Team
+  );
+  console.log(
+    "   " +
+      quarterFinalMatches[1][0].Team +
+      " - " +
+      quarterFinalMatches[1][1].Team
+  );
+  console.log(
+    "   " +
+      quarterFinalMatches[2][0].Team +
+      " - " +
+      quarterFinalMatches[2][1].Team
+  );
+  console.log(
+    "   " +
+      quarterFinalMatches[3][0].Team +
+      " - " +
+      quarterFinalMatches[3][1].Team
+  );
 }
 
 module.exports = calculateProbability;
